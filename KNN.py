0f396401c34b8d590e8cf526e3d5bb949b328470
jay-nagaria <34761045+jay-nagaria@users.noreply.github.com>
2018-02-26 20:45:37 -0600

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -2,7 +2,6 @@
 import csv
 import math
 import operator
-import random
 
 '''...............This program is personalized according to the IRIS dataset............... '''
 '''..................if u wish to change the dataset please make the changes acordingly.................'''
@@ -13,19 +12,16 @@ testSet=[]
 
 
 
-with open('G:\Programs\ATNTFaceImages400.txt', 'r') as csvfile:
+with open('(dataset path)', 'r') as csvfile:
     lines = csv.reader(csvfile)
-    dataset1 = list(lines)
-    dataset=[list(i) for i in zip(*dataset1)]
-    for x in range(len(dataset)):
-        for y in range(1,len(dataset[0])):
+    dataset = list(lines)
+    for x in range(len(dataset)-1):
+        for y in range(4):
             dataset[x][y] = float(dataset[x][y])
-            
-        
-        trainingSet.append(dataset[x])
-        if x%3 == 0:
-	            testSet.append(dataset[x])
-
+        if x < 3*(len(dataset)/4):
+            trainingSet.append(dataset[x])
+        else:
+            testSet.append(dataset[x])
                 
 print ('Training set: ' + repr(len(trainingSet)))
 print ('Testing set: ' + repr(len(testSet)))
@@ -34,7 +30,7 @@ print ('Testing set: ' + repr(len(testSet)))
                 
 def Distance(t1,t2,length):
     distance = 0
-    for x in range(1,length):
+    for x in range(length):
         distance += math.pow(float(t1[x])-float(t2[x]),2)
     return math.sqrt(distance)
         
@@ -53,19 +49,18 @@ def myknnclassify(X, test, k):
     for x in range(len(neighbour)):
         predict_class = {}
         if neighbour[x][-1] in predict_class:
-            predict_class[neighbour[x][0]] += 1
+            predict_class[neighbour[x][-1]] += 1
         else:
-            predict_class[neighbour[x][0]] = 1
+            predict_class[neighbour[x][-1]] = 1
             
     predict_class = sorted(predict_class.items(), key=operator.itemgetter(1), reverse=True)
     return predict_class[0][0]
 
 '''----------------------------------Calc accuracy--------------------------------------------'''
-
 def faccuracy(testSet, prediction):
     All = true_prediction = 0
     for x in range(len(testSet)):
-        if testSet[x][0] == prediction[x]:
+        if testSet[x][-1] == prediction[x]:
             true_prediction += 1
         All += 1
     return (true_prediction/All)*100
@@ -74,11 +69,11 @@ def faccuracy(testSet, prediction):
 
 
 predictions=[]
-k = 3
+k = 1
 for x in range(len(testSet)):
-    result = myknnclassify(trainingSet, testSet[x], k)
-    predictions.append(result)
-    print(repr(result)+" - "+repr(testSet[x][0]))
+	result = myknnclassify(trainingSet, testSet[x], k)
+	predictions.append(result)
+    
 accuracy = faccuracy(testSet, predictions)
 print('Accuracy: ' + repr(accuracy) + '%')
 	
